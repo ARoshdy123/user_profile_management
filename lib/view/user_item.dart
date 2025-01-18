970928643adab306cdb3732cb0c370f457066d97
Ahmed Roshdy Ragheb Mohamed <ahmedroshdy6555554@gmail.com>
2025-01-18 22:43:37 +0200

    commit

--- lib/view/user_item.dart
+++ lib/view/user_item.dart
@@ -1,77 +1,90 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:user_profile_management/models/user_model.dart';
 import 'package:user_profile_management/services/user_service.dart';
 
-class UserItem extends StatefulWidget {
-  UserItem({super.key, required this.userModel});
-  Users userModel;
+import 'update_user_page.dart';
 
-  @override
-  State<UserItem> createState() => _UserItemState();
-}
+class UserItem extends StatelessWidget {
+  final Users userModel;
+  final VoidCallback onReload; // Callback to reload the user list to run the cached users
 
-class _UserItemState extends State<UserItem> {
+  UserItem({super.key, required this.userModel, required this.onReload});
+
+  void deleteUser(BuildContext context) async {
+    bool confirmDelete = await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Delete User"),
+        content: Text("Are you sure you want to delete this user? ${userModel.name}?"),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context, false),
+            child: Text("No"),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, true),
+            child: Text("Yes", style: TextStyle(color: Colors.red)),
+          ),
+        ],
+      ),
+    );
+
+    if (confirmDelete) {
+      await UserService().deleteUser(context, userModel.id.toString());
+      onReload();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("${userModel.name} has been deleted.")),
+      );
+    }
+  }
+  void editUser(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => UpdateUserPage(user: userModel), // Pass the user to be updated
+      ),
+    );
+
+    if (result == true) {
+      onReload(); // Reload the user list after updating
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("${userModel.name}'s data has been updated.")),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: ListTile(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-          tileColor: Color(0xffcdd9ec),
-          leading: Text(
-            "${widget.userModel.id}",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-          ),
-          trailing: IconButton(
-              onPressed: ()async {
-                List<Users> users = [];
-               await  deleteUser(widget.userModel.id.toString());
-             setState(() {
-             });
-              },
-              icon: Icon(
-                Icons.delete,
-                color: Colors.redAccent,
-              )),
-          title: Text(widget.userModel.name,
-          style: TextStyle(
-            fontSize: 25
-          ),),
-          subtitle: Text(widget.userModel.email,
-          style: TextStyle(
-            fontSize: 20
-          ),),
-          onTap: () {}
-
-          /// todo => Navigator.push(
-          //   context,
-          //   MaterialPageRoute(
-          //     builder: (_) => EditUserPage(user: user),
-          //   ),
-          // ).then((value) => getUsers()),
-          ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        tileColor: Color(0xffcdd9ec),
+        leading: Text(
+          "${userModel.id}",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
+        trailing: Row(mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              onPressed: () => editUser(context), // Call edit method
+              icon: Icon(Icons.edit, color: Colors.blueAccent),
+            ),
+            IconButton(
+              onPressed: () => deleteUser(context), // Call delete method
+              icon: Icon(Icons.delete, color: Colors.redAccent),
+            ),
+          ],
+        ),
+        title: Text(
+          userModel.name,
+          style: TextStyle(fontSize: 25),
+        ),
+        subtitle: Text(
+          userModel.email,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
     );
-  }
-  deleteUser(String userId){
-    showDialog(context: context, builder: (context) {
-      return AlertDialog(
-        content: Text("Are you sure you want to delete this user?"),
-      actions: [
-        TextButton(onPressed: ()async{
-          await UserService().deleteUser(context,userId);
-          Navigator.pop(context);
-          setState(() {
-          });
-        }, child: Text("Yes")),
-        TextButton(onPressed: (){
-          Navigator.pop(context);
-        }, child: Text("No")),
-      ],
-      );
-    },);
   }
 }
